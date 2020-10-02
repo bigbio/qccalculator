bd5eaf73bd896b0c4b7e53247c9bba1f4abf7ff4
yperez <ypriverol@gmail.com>
2020-10-02 15:27:09 +0100

    minor tests

--- tests/test_basicqc.py
+++ tests/test_basicqc.py
@@ -2,6 +2,7 @@ import unittest
 import pyopenms as oms
 
 import basicqc
+import utils
 
 
 class MyTestCase(unittest.TestCase):
@@ -31,6 +32,45 @@ class MyTestCase(unittest.TestCase):
     self.assertTrue(len(peps) == 20)
 
     self.assertTrue('RNPxlSearch' in pros[0].getIdentifier())
+
+    for pepi in peps:
+      if not pepi.empty():
+        # TODO if not decoy and not under threshold
+        psm_count = len(pepi.getHits())
+        print(psm_count)
+        for psm in pepi.getHits():
+          print(psm.getSequence().toString())
+        if pepi.getHits():
+          print(pepi.getHits()[0].getSequence().toString())
+
+    for pepi in peps:
+      pid = utils.pep_native_id(pepi)
+      if pepi.getHits():
+        tmp = pepi.getHits()[0]  # TODO apply custom filters and also consider 'pass_threshold'
+        print(pepi.getRT())
+        print(tmp.getCharge())
+        print(tmp.getScore())
+
+        tw = (tmp.getSequence().getMonoWeight(0, 0) + tmp.getCharge() * oms.Constants.PROTON_MASS_U) / tmp.getCharge()
+        dppm = utils.getMassDifference(tw, pepi.getMZ(), True)
+        print(pepi.getRT())
+        print(pepi.getMZ())
+        print(dppm)
+        print(utils.getMassDifference(tw, pepi.getMZ(), False))
+        print(utils.getMassDifference(tw, pepi.getMZ(), False))
+
+        print(pepi.getRT())
+        print(pepi.getRT())
+        print(tmp.getSequence().toString().lstrip().rstrip())
+        print(tmp.getMetaValue('target_decoy').lower() == 'target')
+        print(pid)
+
+
+    # Iterate over all protein hits
+    for hit in pros[0].getHits():
+      print("Protein hit accession:", hit.getAccession())
+      print("Protein hit sequence:", hit.getSequence())
+      print("Protein hit score:", hit.getScore())
 
 
 
